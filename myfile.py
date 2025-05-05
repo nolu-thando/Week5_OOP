f11d22126101f26fc1eb3aed056d157f1726845b
Noluthando Mphande <noluthandowendy2013@gmail.com>
2025-05-05 11:29:43 +0200

    question 1 completed

--- myfile.py
+++ myfile.py
@@ -1,17 +1,27 @@
+# Base class
 class Character:
-    def __init__(self,name, origin):
+    def __init__(self, name, origin):
         self.name = name
         self.origin = origin
 
-        def introduce (self):
-            return f "I am {self.name} from {self.origin}!"
+    def introduce(self):
+        return f"I am {self.name} from {self.origin}!"
 
-            #subclass
-            class Superhero (Character):
-                def __init__(self, name, origin,power, weakness):
-                    super().__init__(name, origin)
-                    self. __power = power
-                    self . __weakness = weakness
-                
-                def use_power(self):
-                    return f "{self.name} uses {self.__power}!"
+# Subclass 
+class Superhero(Character):
+    def __init__(self, name, origin, power, weakness):
+        super().__init__(name, origin)
+        self.__power = power      # Encapsulated attribute
+        self.__weakness = weakness
+
+    def use_power(self):
+        return f"{self.name} uses {self.__power}!"
+
+    def reveal_weakness(self):
+        return f"{self.name}'s weakness is {self.__weakness}."
+
+# Example usage
+hero1 = Superhero("Catwoman", "New York City", "Super Strength", "Bright Light")
+print(hero1.introduce())
+print(hero1.use_power())
+print(hero1.reveal_weakness())
